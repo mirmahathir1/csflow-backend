e929be56738821436232a2bfa3dc1c30070ea345
avijit <avijitbiswas1217@gmail.com>
2021-04-13 11:53:43 +0600

    Implementing /archive/resource Route to fetch all drive links

--- models/resourcearchive.js
+++ models/resourcearchive.js
@@ -1,5 +1,5 @@
 const db = require("../db");
-//const sql = require('mysql2');
+
 
 module.exports = class Resourcearchive {
     constructor(resourcearchive) {
@@ -19,17 +19,13 @@ static async fetchAll() {
         batches.push(Object.values(row2[0][j]));
     }
 
-
     let k;
     for(k=0;k<batches.length;k++){
         let batch_no = batches[k][0]
-        console.log(batch_no);
-        let row = await db.execute('SELECT Level,Term,Link FROM resourcearchive WHERE BatchID = ${batch_no}');
 
-        let count = await db.execute('SELECT COUNT(ID) FROM resourcearchive WHERE BatchID = ${batch_no}');
+        let row = await db.execute(`SELECT Level,Term,Link FROM resourcearchive WHERE BatchID = ${batch_no};`);
 
-        let obj = count[0][0];
-        let c = Object.values(obj);
+        let c = row[0].length;
         if(row[0].length===0){
             return null;
         }
