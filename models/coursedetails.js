4ab568c17e77bdb68c5c508c2d32c9e87392433c
avijit <avijitbiswas1217@gmail.com>
2021-04-23 22:39:14 +0600

    added Distinct keyword

--- models/coursedetails.js
+++ models/coursedetails.js
@@ -1,7 +1,7 @@
 const db = require('../db');
 module.exports = class Coursedetails {
     static async findCourses(batchID) {
-        let result = await db.execute(`SELECT c.CourseNo, c.Title
+        let result = await db.execute(`SELECT DISTINCT c.CourseNo, c.Title
                                        FROM coursedetail c
                                                 JOIN projectarchive p ON c.ID = p.CourseID
                                        WHERE p.BatchID = ${batchID};`);
